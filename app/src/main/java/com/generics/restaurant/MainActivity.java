a28123e67c74d17a66ea5fbb0081269168d548af
und1f3n3d <undefned4@gmail.com>
2019-08-18 18:16:21 +0300

    added toolbar

--- app/src/main/java/com/generics/restaurant/MainActivity.java
+++ app/src/main/java/com/generics/restaurant/MainActivity.java
@@ -2,15 +2,27 @@ package com.generics.restaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle("");
+        }
     }
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
